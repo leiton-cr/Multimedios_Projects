88a0faba2cba4e86fef0dc9c8ccd0032ed0d887a
leiton-cr <80017848+leiton-cr@users.noreply.github.com>
2022-05-26 21:53:42 -0600

    Agrego prueba inicial

---
+++ 01.Test/offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1653623234,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/sprite-sheet0.png",
+		"images/sprite-sheet1.png",
+		"images/sprite-sheet2.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Multimedios_Projects
 Proyectos en construct 2
+
+
+01. Primer prueba
+https://leiton-cr.github.io/Multimedios_Projects/01.Test
